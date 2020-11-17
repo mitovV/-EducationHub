cca281ae053f75b16a2061214257ea0aaa0c1b9c
CodingMania <vasilmitovv@gmail.com>
2020-11-17 16:45:06 +0200

    Refactoring

--- Web/EducationHub.Web.ViewModels/Administration/CourseAdminViewModel.cs
+++ Web/EducationHub.Web.ViewModels/Administration/CourseAdminViewModel.cs
@@ -3,24 +3,15 @@
     using System;
 
     using AutoMapper;
-    using EducationHub.Data.Models;
-    using EducationHub.Services.Mapping;
+    using Data.Common.Models;
+    using Data.Models;
+    using Services.Mapping;
 
-    public class CourseAdminViewModel : IMapFrom<Course>, IHaveCustomMappings
+    public class CourseAdminViewModel : BaseDeletableModel<string>, IMapFrom<Course>, IHaveCustomMappings
     {
         public string Title { get; set; }
 
         public string Description { get; set; }
-
-        public string Id { get; set; }
-
-        public DateTime CreatedOn { get; set; }
-
-        public DateTime? ModifiedOn { get; set; }
-
-        public bool IsDeleted { get; set; }
-
-        public DateTime? DeletedOn { get; set; }
 
         public string User { get; set; }
 
