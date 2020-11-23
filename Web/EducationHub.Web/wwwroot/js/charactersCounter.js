b3c7d7de8397bf44761c4c31e1cc8b8702d0ed44
CodingMania <vasilmitovv@gmail.com>
2020-11-23 10:41:37 +0200

    Improved characterCounter logic

--- Web/EducationHub.Web/wwwroot/js/charactersCounter.js
+++ Web/EducationHub.Web/wwwroot/js/charactersCounter.js
@@ -1,5 +1,5 @@
 ﻿(function () {
     $(document).ready(function () {
-        $('input#Name, input#PictureUrl').characterCounter();
+        $('input.character-counter').characterCounter();
     });
 })();
