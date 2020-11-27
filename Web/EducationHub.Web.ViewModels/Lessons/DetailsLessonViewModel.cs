27fa0daa70b84f6833bb7bb586fb04c336801138
CodingMania <vasilmitovv@gmail.com>
2020-11-27 12:19:13 +0200

    Extend logic for video Url

--- Web/EducationHub.Web.ViewModels/Lessons/DetailsLessonViewModel.cs
+++ Web/EducationHub.Web.ViewModels/Lessons/DetailsLessonViewModel.cs
@@ -1,8 +1,10 @@
 ﻿namespace EducationHub.Web.ViewModels.Lessons
 {
+    using System;
     using System.Linq;
-
+    using System.Text.RegularExpressions;
     using Data.Models;
+    using Ganss.XSS;
     using Services.Mapping;
 
     public class DetailsLessonViewModel : IMapFrom<Lesson>
@@ -11,6 +13,16 @@
 
         public string VideoUrl { get; set; }
 
-        public string ViewModelVideoUrl => this.VideoUrl.Split("/").Reverse().ToArray()[0];
+        public string UserUsername { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string UserPictureUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string SanitiedDescription => new HtmlSanitizer().Sanitize(this.Description);
+
+        public string ViewModelVideoUrl => new Regex("v=[^&]+&").Match(this.VideoUrl).Value.TrimStart('v', '=').TrimEnd('&') == string.Empty ? this.VideoUrl.Split("/").Reverse().ToArray()[0] : new Regex("v=[^&]+&").Match(this.VideoUrl).Value.TrimStart('v', '=').TrimEnd('&');
     }
 }
