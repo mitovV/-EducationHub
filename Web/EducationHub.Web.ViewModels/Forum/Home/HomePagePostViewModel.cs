cbd8fec740c2f25b1d46e18d633f9650689f79fe
CodingMania <vasilmitovv@gmail.com>
2020-12-11 16:06:12 +0200

    Fix bug

--- Web/EducationHub.Web.ViewModels/Forum/Home/HomePagePostViewModel.cs
+++ Web/EducationHub.Web.ViewModels/Forum/Home/HomePagePostViewModel.cs
@@ -15,6 +15,8 @@
 
         public string UserUserName { get; set; }
 
+        public int CategoryId { get; set; }
+
         public string CategoryName { get; set; }
 
         public string UserPictureUrl { get; set; }
