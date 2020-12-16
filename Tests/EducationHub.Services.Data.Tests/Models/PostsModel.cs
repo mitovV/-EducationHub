789d118b1d0c092120d9ff85bdf19324096add2d
CodingMania <vasilmitovv@gmail.com>
2020-12-16 19:13:27 +0200

    Add tests for PostsService

--- Tests/EducationHub.Services.Data.Tests/Models/PostsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EducationHub.Services.Data.Tests.Models
-{
-    using System.Collections.Generic;
-
-    public class PostsModel
-    {
-        public IEnumerable<PostModel> Posts { get; set; }
-    }
-}
--- Tests/EducationHub.Services.Data.Tests/PostsServiceTests.cs
+++ Tests/EducationHub.Services.Data.Tests/PostsServiceTests.cs
@@ -1,7 +1,6 @@
 ﻿namespace EducationHub.Services.Data.Tests
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using System.Threading.Tasks;
@@ -17,13 +16,18 @@
 
     public class PostsServiceTests : IDisposable
     {
+        private const string Title = "Test";
+        private const string Content = "Test";
+        private const string UserId = "Test";
+        private const int CategoryId = 1;
+
         private readonly EfRepository<Post> postsRepository;
         private readonly IPostsService postsService;
 
         public PostsServiceTests()
         {
             var optionsBuilder = new DbContextOptionsBuilder<EducationHubDbContext>()
-               .UseInMemoryDatabase("test");
+               .UseInMemoryDatabase("db");
 
             var dbCntext = new EducationHubDbContext(optionsBuilder.Options);
             dbCntext.Database.EnsureDeleted();
@@ -42,7 +46,7 @@
             // Arrange
 
             // Act
-            await this.postsService.CratePostAsync("Test", "Test", "1", 1);
+            await this.postsService.CratePostAsync(Title, Content, UserId, CategoryId);
             var expectedCount = 1;
             var actualCount = this.postsRepository.All().Count();
 
@@ -58,37 +62,93 @@
         public async Task GetByIdAsyncShluldReturnCorrectValue()
         {
             // Arrange
-            var title = "test";
-            var content = "test";
 
             // Act
-            await this.postsService.CratePostAsync(title, content, "1", 2);
+            await this.postsService.CratePostAsync(Title, Content, UserId, CategoryId);
             var postId = this.postsRepository.All().FirstOrDefault().Id;
 
             var post = await this.postsService.GetByIdAsync<PostModel>(postId);
 
             // Assert
             Assert.NotNull(post);
-            Assert.Equal(title, post.Title);
-            Assert.Equal(content, post.Content);
+            Assert.Equal(Title, post.Title);
+            Assert.Equal(Content, post.Content);
         }
 
-        // TODO: Not work
+        [Fact]
+        public async Task GetByIdAsyncShluldReturnNull()
+        {
+            // Arrange
+
+            // Act
+            var post = await this.postsService.GetByIdAsync<PostModel>(1);
+
+            // Assert
+            Assert.Null(post);
+        }
+
         [Fact]
         public async Task GetPostsAsyncShouldReturnCorrectValues()
         {
             // Arrange
-            var title = "test";
-            var content = "test";
+            var secondTitle = "test2";
+            var secondContent = "test2";
 
             // Act
-            await this.postsService.CratePostAsync(title, content, "1", 2);
-            await this.postsService.CratePostAsync(title, content, "1", 2);
+            await this.postsService.CratePostAsync(Title, Content, UserId, CategoryId);
+            await this.postsService.CratePostAsync(secondTitle, secondContent, UserId, CategoryId);
 
-            var posts = await this.postsService.GetPostsAsync<PostsModel>(2);
+            var expectedCount = 2;
+
+            var posts = await this.postsService.GetPostsAsync<PostModel>(expectedCount);
+
+            var actualCount = posts.Count();
 
             // Assert
+            Assert.Equal(expectedCount, actualCount);
+        }
 
+        [Fact]
+        public async Task GetPostsAsyncShouldReturnDataInCorrectOrder()
+        {
+            // Arrange
+            var secondTitle = "test2";
+            var secondContent = "test2";
+
+            // Act
+            await this.postsService.CratePostAsync(Title, Content, UserId, CategoryId);
+            await this.postsService.CratePostAsync(secondTitle, secondContent, UserId, CategoryId);
+
+            var posts = await this.postsService.GetPostsAsync<PostModel>(2);
+
+            var expectedTitle = "test2";
+            var actualTitle = posts.FirstOrDefault().Title;
+
+            // Assert
+            Assert.Equal(expectedTitle, actualTitle);
+        }
+
+        [Fact]
+        public async Task GetPostsByCategoryAsyncShouldReturtCorrectValues()
+        {
+            // Arrange
+            var secondTitle = "test2";
+            var secondContent = "test2";
+
+            var thirdTitle = "test3";
+            var thirdContent = "test3";
+
+            // Act
+            await this.postsService.CratePostAsync(Title, Content, UserId, CategoryId);
+            await this.postsService.CratePostAsync(secondTitle, secondContent, UserId, CategoryId);
+            await this.postsService.CratePostAsync(thirdTitle, thirdContent, UserId, 3);
+
+            var posts = await this.postsService.GetPostsByCategoryAsync<PostModel>(CategoryId);
+            var expectedCount = 2;
+            var actualCount = posts.Count();
+
+            // Assert
+            Assert.Equal(expectedCount, actualCount);
         }
 
         public void Dispose()
