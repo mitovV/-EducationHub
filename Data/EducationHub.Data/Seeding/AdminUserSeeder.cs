8d9d564030aa4bc3288f2c66c100bcf94e48bb54
Vasil <vasilmitovv@gmail.com>
2023-12-17 17:32:13 +0200

    Add default picture URL admin data seeder

--- Data/EducationHub.Data/Seeding/AdminUserSeeder.cs
+++ Data/EducationHub.Data/Seeding/AdminUserSeeder.cs
@@ -24,6 +24,7 @@
             {
                 Email = GlobalConstants.AdministratorEmail,
                 UserName = GlobalConstants.AdministratorEmail,
+                PictureUrl = "https://res.cloudinary.com/dyxeqmpgh/image/upload/v1702826833/avatardefault_92824_svggzm.png",
             };
 
             var password = GlobalConstants.AdministratorPassword;
